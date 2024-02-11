41ac87be82aaf21435ca4267cf09f3af2dacd799
Vitalii Kolesov <vitalii_kolesov@epam.com>
2024-02-12 01:24:39 +0400

    init

---
+++ src/test/java/io/practice/springfoundationhomework/SpringFoundationHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.practice.springfoundationhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoundationHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
