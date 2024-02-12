badbd845fc7fb44c7266e16e09241096763a51a2
Vitalii Kolesov <vitalii_kolesov@epam.com>
2024-02-12 17:52:47 +0400

    feat: task 2

--- src/main/java/io/practice/springfoundationhomework/DataSourceConfig.java
+++ src/main/java/io/practice/springfoundationhomework/DataSourceConfig.java
@@ -1,5 +1,7 @@
 package io.practice.springfoundationhomework;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 @Configuration
+@Slf4j
 public class DataSourceConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "use-custom-datasource", matchIfMissing = true)
     public DataSource getDataSource() {
+        log.info("Creating custom datasource");
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.h2.Driver");
         dataSourceBuilder.url("jdbc:h2:mem:test");
